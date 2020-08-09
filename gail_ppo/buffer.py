7e8bc0f06be67f2cb2b47ab522b439b6f9c7d2f0
Toshiki Watanabe <kuboy2482@gmail.com>
2020-08-10 03:23:32 +0900

    add buffer

---
+++ gail_ppo/buffer.py
@@ -0,0 +1,106 @@
+import numpy as np
+import torch
+
+
+class SirializedBuffer:
+
+    def __init__(self, path, device=torch.device('cuda')):
+        tmp = torch.load(path)
+        self.buffer_size = self._n = tmp['state'].size(0)
+        self.device = device
+
+        self.states = tmp['state'].clone().to(self.device)
+        self.actions = tmp['action'].clone().to(self.device)
+        self.rewards = tmp['reward'].clone().to(self.device)
+        self.dones = tmp['done'].clone().to(self.device)
+        self.next_states = tmp['next_state'].clone().to(self.device)
+
+    def sample(self, batch_size):
+        idxes = np.random.randint(low=0, high=self._n, size=batch_size)
+        return (
+            self.states[idxes],
+            self.actions[idxes],
+            self.rewards[idxes],
+            self.dones[idxes],
+            self.next_states[idxes]
+        )
+
+
+class Buffer(SirializedBuffer):
+
+    def __init__(self, buffer_size, state_shape, action_shape, device):
+        self._n = 0
+        self._p = 0
+        self.buffer_size = buffer_size
+        self.device = device
+
+        self.states = torch.empty(
+            (buffer_size, *state_shape), dtype=torch.float, device=device)
+        self.actions = torch.empty(
+            (buffer_size, *action_shape), dtype=torch.float, device=device)
+        self.rewards = torch.empty(
+            (buffer_size, 1), dtype=torch.float, device=device)
+        self.dones = torch.empty(
+            (buffer_size, 1), dtype=torch.float, device=device)
+        self.next_states = torch.empty(
+            (buffer_size, *state_shape), dtype=torch.float, device=device)
+
+    def append(self, state, action, reward, done, next_state):
+        self.states[self._p].copy_(torch.from_numpy(state))
+        self.actions[self._p].copy_(torch.from_numpy(action))
+        self.rewards[self._p] = float(reward)
+        self.dones[self._p] = float(done)
+        self.next_states[self._p].copy_(torch.from_numpy(next_state))
+
+        self._p = (self._p + 1) % self.buffer_size
+        self._n = min(self._n + 1, self.buffer_size)
+
+    def save(self, path):
+        torch.save({
+            'state': self.states.clone().cpu(),
+            'action': self.actions.clone().cpu(),
+            'reward': self.rewards.clone().cpu(),
+            'done': self.dones.clone().cpu(),
+            'next_state': self.next_states.clone().cpu(),
+        }, path)
+
+
+class RolloutBuffer:
+
+    def __init__(self, buffer_size, state_shape, action_shape, device):
+
+        self._p = 0
+        self.buffer_size = buffer_size
+
+        self.states = torch.empty(
+            (buffer_size + 1, *state_shape), dtype=torch.float, device=device)
+        self.actions = torch.empty(
+            (buffer_size, *action_shape), dtype=torch.float, device=device)
+        self.rewards = torch.empty(
+            (buffer_size, 1), dtype=torch.float, device=device)
+        self.dones = torch.empty(
+            (buffer_size, 1), dtype=torch.float, device=device)
+        self.log_pis = torch.empty(
+            (buffer_size, 1), dtype=torch.float, device=device)
+
+    def append(self, state, action, reward, done, log_pi):
+        self.states[self._p].copy_(torch.from_numpy(state))
+        self.actions[self._p].copy_(torch.from_numpy(action))
+        self.rewards[self._p] = float(reward)
+        self.dones[self._p] = float(done)
+        self.log_pis[self._p] = float(log_pi)
+        self._p = (self._p + 1) % self.buffer_size
+
+    def append_last_state(self, last_state):
+        assert self._p == 0
+        self.states[self.buffer_size].copy_(torch.from_numpy(last_state))
+
+    def get(self):
+        assert self._p == 0
+        return (
+            self.states,
+            self.actions,
+            self.rewards,
+            self.dones,
+            self.log_pis
+        )
